eb044857a3306d0a14eb6e50f78306da1fc8f143
eanucc <45760389+eanucc@users.noreply.github.com>
2023-11-09 22:38:32 +0800

    Update diy-part1-immortal.sh

--- diy-part1-immortal.sh
+++ diy-part1-immortal.sh
@@ -1,4 +1,4 @@
-echo 'src-git-full packages https://github.com/immortalwrt/packages.git^875609b52' >>feeds.conf.default
-echo 'src-git-full luci https://github.com/immortalwrt/luci.git^8d55064f3' >>feeds.conf.default
-echo 'src-git-full routing https://github.com/openwrt/routing.git^d79f2b5' >>feeds.conf.default
-echo 'src-git-full telephony https://github.com/openwrt/telephony.git^920fbc5' >>feeds.conf.default
+# echo 'src-git-full packages https://github.com/immortalwrt/packages.git^875609b52' >>feeds.conf.default
+# echo 'src-git-full luci https://github.com/immortalwrt/luci.git^8d55064f3' >>feeds.conf.default
+# echo 'src-git-full routing https://github.com/openwrt/routing.git^d79f2b5' >>feeds.conf.default
+# echo 'src-git-full telephony https://github.com/openwrt/telephony.git^920fbc5' >>feeds.conf.default
